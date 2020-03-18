6d8556f32681a90d9e420403d2fa776332eb2d0e
Christopher Dishop <christopherdishop@Christophers-iMac.local>
2020-03-18 11:33:08 -0400

    simulation across employees complete

--- analysis/simulations/proposal_simulations.jl
+++ analysis/simulations/proposal_simulations.jl
@@ -289,3 +289,180 @@ end
 
 
 # rq3: more employees
+
+
+
+
+
+
+using DataFrames, DataFramesMeta, Statistics, Distributions, Random, RCall
+
+
+# what is the probability that the employee is in the top 20%
+
+# common parameters
+
+total_walks = 800
+steps = 20
+function initial_value()
+    x = rand(Normal(0,1), 1)[1]
+    return(x)
+end
+
+rq3_df = DataFrame(
+    simulation = [],
+    employee_condition = [],
+    moment_citizen_counts = []
+)
+
+total_sims = 100
+
+for sim in 1:total_sims
+
+    # simulate all random walks
+
+    df = DataFrame()
+
+    for walk in 1:total_walks
+
+        y = zeros(steps)
+        y[1] = initial_value()
+
+        for step in 2:steps
+            y[step] = y[step - 1] + rand(Normal(0,1), 1)[1]
+        end
+
+        insert!(df, walk, y, Symbol(walk))
+    end
+
+
+    # iterate across employee amounts, select that many columns
+
+    employee_conditions = [2, 200, 400, 500, 800]
+    store_it = DataFrame(
+        simulation = zeros(5),
+        employee_condition = zeros(5),
+        moment_citizen_counts = zeros(5)
+    )
+
+    counter = 0
+
+
+    for employee_count in employee_conditions
+         counter = counter + 1
+
+        # pull employee_count (e.g., 2) random columns from a vector
+        use_cols = collect(1:1:employee_count)
+
+        # now select those columns to work with
+        condition_df = @linq df |>
+            select(use_cols)
+
+            @rput condition_df
+            @rput employee_count
+            R"""
+            library(tidyverse)
+            moment_counts <- 0
+
+            for (row_i in 1:20){
+
+                row <- condition_df %>%
+                    slice(row_i)
+
+                percent_use <- round(ceiling(0.2*employee_count))
+
+                max_percent_df <- row %>%
+                    rownames_to_column() %>%
+                    gather(column, value, -rowname) %>%
+                    group_by(rowname) %>%
+                    mutate(rk = rank(-value)) %>%
+                    filter(rk <= percent_use) %>%
+                    arrange(rowname, rk)
+
+                column_contained <- sum(max_percent_df$column == "1")
+
+                if(column_contained >= 1){moment_counts <- moment_counts + 1}
+
+            }
+            """
+            @rget moment_counts
+
+            store_it[counter, :simulation] = sim
+            store_it[counter, :employee_condition] = employee_count
+            store_it[counter, :moment_citizen_counts] = moment_counts
+
+        end
+
+        append!(rq3_df, store_it)
+end
+
+
+
+# condition 1: 2 employees
+
+cond1_df = @linq rq3_df |>
+    where(:employee_condition .== 2)
+
+prob_steps = collect(0:1:steps)
+probability_list_2 = zeros(length(prob_steps))
+
+for prob in prob_steps
+    result = count(
+        k == prob for k in cond1_df.moment_citizen_counts) / length(cond1_df.moment_citizen_counts)
+            probability_list_2[[prob + 1]] .= result
+end
+
+cond1_results = DataFrame(
+    k = [0,1,2,3,4,5,6,7,8,9,10,
+         11,12,13,14,15,16,17,18,19,20],
+    probability = probability_list_2,
+    num_employees = vcat(
+                repeat([2], length(probability_list_2))
+    )
+    )
+
+
+
+# create function
+
+function create_results(employee_number)
+    cond_df = @linq rq3_df |>
+        where(:employee_condition .== employee_number)
+
+    prob_steps = collect(0:1:steps)
+    probability_list_z = zeros(length(prob_steps))
+
+    for prob in prob_steps
+        result = count(
+            k == prob for k in cond_df.moment_citizen_counts) / length(cond_df.moment_citizen_counts)
+                probability_list_z[[prob + 1]] .= result
+    end
+
+    cond_results = DataFrame(
+        k = [0,1,2,3,4,5,6,7,8,9,10,
+                 11,12,13,14,15,16,17,18,19,20],
+        probability = probability_list_z,
+        num_employees = vcat(
+                        repeat([employee_number], length(probability_list_200))
+        )
+        )
+
+        return(cond_results)
+    end
+
+# condition 2: 200 employees
+cond2_results = create_results(200)
+# 400 employees
+cond3_results = create_results(400)
+# 600 employees
+cond4_results = create_results(600)
+# 800 employees
+cond5_results = create_results(800)
+
+
+
+
+r3_output = append!(cond1_results, cond2_results)
+r3_output = append!(r3_output, cond3_results)
+r3_output = append!(r3_output, cond4_results)
+r3_output = append!(r3_output, cond5_results)
