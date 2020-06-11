1eefc94e29921704ba9c1c2f82eeb77ab9fbb9f0
Christopher Dishop <christopherdishop@Christophers-iMac.local>
2020-06-10 23:26:05 -0400

    msu random walk plots

--- analysis & data/pilot/msustudents/analysis/analysis.R
+++ analysis & data/pilot/msustudents/analysis/analysis.R
@@ -2,16 +2,22 @@ library(tidyverse)
 library(tseries)
 library(plm)
 df <- read.csv("../data/cleaned/df.csv")
+
+# remove a few series after visual inspection (e.g., they canceled the program)
+
+nouse <- c("College of Medicine-Anatomy",
+           "College of Social Science-Psychology-Urban Studies",
+           "College of Education-Educational Psychology",
+           "College of Natural Science-Entomology")
+
+df <- df %>%
+  filter(!degree %in% nouse)
+
+# practice once
+
 degrees <- unique(df$degree)
-
-
-
-
 df1 <- df %>%
   filter(degree == degrees[18])
-df1 <- df1 %>%
-  mutate(time = 1:nrow(df1))
-
 
 dfdickey <- plm.data(df1, index = c("file", "time"))
 
@@ -30,8 +36,6 @@ for(i in 1:runs){
   
   dt <- df %>%
     filter(degree == degrees[i])
-  dt <- dt %>%
-    mutate(time = 1:nrow(dt))
   
   dtdickey = plm.data(dt, index = c("file", "time"))
   
@@ -53,3 +57,7 @@ for(i in 1:runs){
 (store_results %>%
   filter(dickey > 0.05) %>%
   count()) / nrow(store_results)
+
+(store_results %>%
+    filter(kpss < 0.05) %>%
+    count()) / nrow(store_results)
--- analysis & data/pilot/msustudents/plots/plotting.R
+++ analysis & data/pilot/msustudents/plots/plotting.R
@@ -2,16 +2,60 @@ library(ggplot2)
 library(tidyverse)
 library(data.table)
 df <- read.csv("../data/cleaned/df.csv")
-table(df$degree)
-
 dt <- data.table(df)
 
 
 
-ggplot(dt[degree == "Statistics"], aes(x = time, y = total, group = degree_id)) +
-         geom_point() + 
-         geom_line()
+ggplot(dt[degree == "College of Natural Science-Statistics"], aes(x = time, y = total, group = degree_id)) +
+         geom_point(alpha = 0.8) + 
+         geom_line(color = "darkblue", alpha = 0.7) + 
+         labs(x = "Time", y = "Number Graduate Students", title = "Statistics") + 
+         theme_bw()
+  
 
+
+
+ggplot(dt, aes(x = time, y = total, group = degree_id)) +
+  geom_point(alpha = 0.8) + 
+  geom_line(color = "darkblue", alpha = 0.7) + 
+  labs(x = "Time", y = "Number Graduate Students", title = "Statistics") + 
+  theme_bw() + 
+  facet_wrap(~degree)
+  
+# series that look weird:
+# Economics
+# Microbiol & Molecular Genetics
+# Zoology
+# Biochemistry & Molecular Biol
+
+unique(df$degree)
+
+ggplot(dt[degree == "College of Business-Economics"], aes(x = time, y = total, group = degree_id)) +
+  geom_point(alpha = 0.8) + 
+  geom_line(color = "darkblue", alpha = 0.7) + 
+  labs(x = "Time", y = "Number Graduate Students") + 
+  theme_bw()
+
+df %>%
+  filter(degree == "Economics")
+
+ggplot(dt[degree == "College of Natural Science-Microbiol & Molecular Genetics"], aes(x = time, y = total, group = degree_id)) +
+  geom_point(alpha = 0.8) + 
+  geom_line(color = "darkblue", alpha = 0.7) + 
+  labs(x = "Time", y = "Number Graduate Students") + 
+  theme_bw()
+
+df %>%
+  filter(degree == "Microbiol & Molecular Genetics")
+
+ggplot(dt[degree == "College of Natural Science-Zoology"], aes(x = time, y = total, group = degree_id)) +
+  geom_point(alpha = 0.8) + 
+  geom_line(color = "darkblue", alpha = 0.7) + 
+  labs(x = "Time", y = "Number Graduate Students") + 
+  theme_bw()
+
+df %>%
+  filter(degree == "Zoology")
 
 
 
