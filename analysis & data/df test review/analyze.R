f6c52fc8ea662a2aa0baca5a8dcd68c314ca7598
Christopher Dishop <christopherdishop@Christophers-iMac.local>
2020-09-03 09:57:31 -0400

    nobody looks for random walks in. explortatory analysis in

--- analysis & data/df test review/analyze.R
+++ analysis & data/df test review/analyze.R
@@ -17,3 +17,17 @@ df %>% filter(longitudinal == "yes") %>%
   filter(steps > 2) %>% 
   select(steps) %>% 
   arrange(steps)
+
+
+
+df %>% filter(longitudinal == "yes") %>% 
+  filter(steps > 2) %>% 
+  summarize(
+    sum = sum(randomtest == "no")
+  )
+
+df %>% filter(longitudinal == "yes") %>% 
+  filter(steps > 2) %>% 
+  summarize(
+    sum = sum(randomtest == "yes")
+  )
